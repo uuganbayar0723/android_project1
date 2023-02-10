d77e14d5ad2024279e8118583c82101b92d0084b
uuganbayar <uuganbayar.z@unitel.mn>
2023-02-10 13:43:36 +0800

    android navigation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import com.google.android.material.navigation.NavigationView;
 
 /*
  * Main Activity class that loads {@link MainFragment}.
@@ -22,44 +30,61 @@ public class MainActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
 //                    .replace(R.id.webView, new MainFragment())
 //                    .commitNow();
 //        }
-        webview = (WebView) findViewById(R.id.webView);
-        spinner = (ProgressBar)findViewById(R.id.progressBar);
-        webview.setWebViewClient(new CustomWebViewClient());
-
-//        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webview.loadUrl("file:///android_asset/index.html");
+//        webview = (WebView) findViewById(R.id.webView);
+//        spinner = (ProgressBar)findViewById(R.id.progressBar);
+//        webview.setWebViewClient(new CustomWebViewClient());
+//
+////        webview.getSettings().setJavaScriptEnabled(true);
+//        webview.getSettings().setDomStorageEnabled(true);
+//        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+//        webview.loadUrl("file:///android_asset/index.html");
     }
-    private class CustomWebViewClient extends WebViewClient {
-
-        @Override
-        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
-            webview.setVisibility(webview.INVISIBLE);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-
-            spinner.setVisibility(View.GONE);
-
-            view.setVisibility(webview.VISIBLE);
-            super.onPageFinished(view, url);
-
-        }
-
-    }
-    @Override
-    public void onBackPressed(){
-        if(webview != null && webview.canGoBack()){
-            webview.goBack();
-        }else{
-            super.onBackPressed();
-        }
-    }
+//    private class CustomWebViewClient extends WebViewClient {
+//
+//        @Override
+//        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+//            webview.setVisibility(webview.INVISIBLE);
+//        }
+//
+//        @Override
+//        public void onPageFinished(WebView view, String url) {
+//
+//            spinner.setVisibility(View.GONE);
+//
+//            view.setVisibility(webview.VISIBLE);
+//            super.onPageFinished(view, url);
+//
+//        }
+//
+//    }
+//    @Override
+//    public void onBackPressed(){
+//        if(webview != null && webview.canGoBack()){
+//            webview.goBack();
+//        }else{
+//            super.onBackPressed();
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/myapplication/ProfileFragment.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.fragment.app.Fragment;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class ProfileFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ContactFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContactFragment newInstance(String param1, String param2) {
+        ContactFragment fragment = new ContactFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        WebView webView = (WebView)v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        return v;
+    }
+
+    }
